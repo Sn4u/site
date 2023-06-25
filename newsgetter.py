46e850b5eb392295eeeaa0e3f035ebdee569b139
bones <bones@null.com>
2023-06-25 16:57:45 +0100

    make newstape clickable

--- newsgetter.py
+++ newsgetter.py
@@ -5,6 +5,6 @@ def get_news():
     NewsFeed = feedparser.parse("https://feeds.skynews.com/feeds/rss/world.xml")
     stories = []
     for entry in NewsFeed.get("entries"):
-        stories.append(entry.get("title")+ ". ")
+        stories.append((entry.get("title")+ ". ", entry.get("links")[0].get("href")))
         random.shuffle(stories)
     return stories
